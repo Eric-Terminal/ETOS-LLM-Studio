80d924dfbc71259c801a66c6a9a8a8549c6c6194
Eric-Terminal <121368508+Eric-Terminal@users.noreply.github.com>
2025-09-05 18:42:17 +0800

    Initial Commit

---
+++ ETOS LLM Studio Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ETOS LLM Studio Watch App
+//
+//  Created by Eric on 2025/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ETOS LLM Studio Watch App/ETOS_LLM_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETOS_LLM_StudioApp.swift
+//  ETOS LLM Studio Watch App
+//
+//  Created by Eric on 2025/9/5.
+//
+
+import SwiftUI
+
+@main
+struct ETOS_LLM_Studio_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
